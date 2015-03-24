13d252ddb3b6471ac37db205c5582b5f6043bf84
Stian Hanger <pdnagilum@gmail.com>
2015-03-24 16:52:16 +0100

    Add execution for MessageBox and Process

--- WhenPressTrayApp/Classes/JavascriptExecutor.cs
+++ WhenPressTrayApp/Classes/JavascriptExecutor.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Microsoft.ClearScript;
+using Microsoft.ClearScript.V8;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 
 namespace WhenPressTrayApp {
 	public class JavascriptExecutor {
+		private V8ScriptEngine engine;
 		private readonly ConfigEntry configEntry;
 		private string javascriptSource;
 
@@ -15,6 +19,85 @@ namespace WhenPressTrayApp {
 
 			// Read the source of the given script file to memory.
 			this.readScriptSourceFile();
+
+			// Attempt to init a V8 script engine and execite the loaded script.
+			if (!string.IsNullOrWhiteSpace(this.javascriptSource))
+				this.executeScript();
+		}
+
+		/// <summary>
+		/// Attempt to init a V8 script engine and execite the loaded script.
+		/// </summary>
+		private void executeScript() {
+			try {
+				engine = new V8ScriptEngine();
+			}
+			catch (Exception ex) {
+				MessageBox.Show(
+					"Could not init a V8 engine.\r\n\r\n" +
+					ex.Message,
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+			}
+
+			if (engine == null)
+				return;
+
+			// Inject all host objects that are to be available to the script.
+			this.prepareHostObjects();
+
+			// Execute the loaded script.
+			try {
+				engine.Execute(this.javascriptSource);
+			}
+			catch (ScriptEngineException ex) {
+				MessageBox.Show(
+					"Error while running script!\r\n\r\n" +
+					this.configEntry.ScriptFile + "\r\n\r\n" +
+					ex.Message,
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+			}
+			catch (Exception ex) {
+				MessageBox.Show(
+					"Error while running script!\r\n\r\n" +
+					this.configEntry.ScriptFile + "\r\n\r\n" +
+					ex.Message,
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+			}
+		}
+
+		/// <summary>
+		/// Inject all host objects that are to be available to the script.
+		/// </summary>
+		private void prepareHostObjects() {
+			this.engine.AddHostType(
+				"Console",
+				typeof(Console));
+
+			this.engine.AddHostType(
+				"MessageBoxButtons",
+				typeof(MessageBoxButtons));
+
+			this.engine.AddHostType(
+				"MessageBoxIcon",
+				typeof(MessageBoxIcon));
+
+			this.engine.AddHostType(
+				"DialogResult",
+				typeof(DialogResult));
+
+			this.engine.AddHostType(
+				"ProcessWindowStyle",
+				typeof(ProcessWindowStyle));
+
+			this.engine.AddHostObject(
+				"WhenPress",
+				new JavascriptHostObject());
 		}
 
 		/// <summary>
@@ -44,6 +127,82 @@ namespace WhenPressTrayApp {
 					MessageBoxButtons.OK,
 					MessageBoxIcon.Error);
 			}
+		}
+	}
+
+	/// <summary>
+	/// Object available inside the script.
+	/// </summary>
+	public class JavascriptHostObject {
+		/// <summary>
+		/// Display a message-box with just a message.
+		/// </summary>
+		public DialogResult ShowMessageBox(
+			object message) {
+			return MessageBox.Show(
+				message.ToString());
+		}
+
+		/// <summary>
+		/// Display a message-box with message and title.
+		/// </summary>
+		public DialogResult ShowMessageBox(
+			object message,
+			object title) {
+			return MessageBox.Show(
+				message.ToString(),
+				title.ToString());
+		}
+
+		/// <summary>
+		/// Display a message-box with message, title, and buttons.
+		/// </summary>
+		public DialogResult ShowMessageBox(
+			object message,
+			object title,
+			MessageBoxButtons buttons) {
+			return MessageBox.Show(
+				message.ToString(),
+				title.ToString(),
+				buttons);
+		}
+
+		/// <summary>
+		/// Display a message-box with message, title, buttons, and an icon.
+		/// </summary>
+		public DialogResult ShowMessageBox(
+			object message,
+			object title,
+			MessageBoxButtons buttons,
+			MessageBoxIcon icon) {
+			return MessageBox.Show(
+				message.ToString(),
+				title.ToString(),
+				buttons,
+				icon);
+		}
+
+		/// <summary>
+		/// Attempt to run a file on the system.
+		/// </summary>
+		public void Start(string filename) {
+			this.Start(
+				filename,
+				null);
+		}
+
+		/// <summary>
+		/// Attempt to run a file on the system with arguments.
+		/// </summary>
+		public void Start(string filename, string arguments) {
+			var process = new Process {
+				StartInfo = new ProcessStartInfo {
+					FileName = filename,
+					Arguments = arguments
+				}
+			};
+
+			process.Start();
 		}
 	}
 }
