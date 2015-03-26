fcd2c7a392e8db44ad3c5ec619fdbc2c022057bc
Stian Hanger <pdnagilum@gmail.com>
2015-03-26 15:07:18 +0100

    Add script to bring a window to the foreground, or start it.

---
+++ Scripts/bringWindowToForeground.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * Brings a window to the foreground or starts the app if not found.
+ *
+ * @author
+ * Stian Hanger <pdnagilum@gmail.com>
+ */
+
+'use strict';
+
+var title     = WhenPress.GetConfigValue('title'),
+    fileName  = WhenPress.GetConfigValue('fileName'),
+    handle    = null,
+    processes = WhenPress.GetProcesses();
+
+processes.forEach(function (process) {
+  if (process.MainWindowTitle.indexOf(title) === -1)
+    return;
+
+  handle = process.MainWindowHandle;
+});
+
+if (handle)
+  WhenPress.FocusWindowByTitle(title);
+else
+  WhenPress.Start(fileName);
